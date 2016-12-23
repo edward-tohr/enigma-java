a7e955a3a80f274e2958f324bbc5b2ad6669ae44
edward-tohr <edward.tohr@gmail.com>
2016-12-23 12:02:11 -0500

    Probably made more efficient?
    Stopped the program from creating new rotors each time a new message was
    parsed.

--- Main.java
+++ Main.java
@@ -4,12 +4,14 @@ public class Main {
 	boolean loop = true;
 		
 			   Enigma mainEnigma = new Enigma();
-			   while (loop){
+			   
 			   Rotor rot1 = new Rotor(mainEnigma.r1);
 			   Rotor rot2 = new Rotor(mainEnigma.r2);
 			   Rotor rot3 = new Rotor(mainEnigma.r3);
 			   Rotor refl = new Rotor(mainEnigma.ref1);
 			   Rotor plug = new Rotor(mainEnigma.plugb);
+
+			   while (loop){
 		      loop = mainEnigma.enigma(rot1, rot2, rot3, refl, plug);
 		   }
 		   }
